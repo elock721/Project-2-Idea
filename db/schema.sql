d70cad3561f010a65df452b32085ef4061f3b76f
Mozart <coreyspratlan@gmail.com>
2020-07-29 11:39:00 -0500

    added more seeds

--- db/schema.sql
+++ db/schema.sql
@@ -14,6 +14,15 @@ CREATE TABLE foodtruck_name (
 INSERT INTO foodtruck_name (name, cuisine, neighborhood)
 VALUES ("Veracruz All Natural", "Mexican", "4208 Manchaca Rd. 78704"), 
 ("Gordoughs Big Fat Donuts", "American", "1503 South First Street 78704");
+("Con Madre Kitchen", "Mexican", "628 E Oltorf St"),
+("Pueblo Viejo Taco Truck", "Mexican", "121 Pickle Rd"),
+("Mama Mal's Italian Cuisine Food Truck", "Southern Italian", "1320 S Lamar Blvd"),
+("Tamale Addiction", "Mexican", "2340 S Lamar Blvd"),
+("IL Saporis Italian", "Italian", "603 W Live Oak St"),
+("Luke's Inside Out", "Sandwich", "1109 S Lamar Blvd"),
+("Pitalicious", "Mediterranean", "1503 S Congress Ave"),
+("Cannone Cucina Italiana", "Italian", "1720 Barton Springs Rd");
+
 
 
 
